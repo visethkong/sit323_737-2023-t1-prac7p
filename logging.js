7ff56b0a483a8ae5357e58c69a008d7ae4896e4e
viseth <52366670+visethkong@users.noreply.github.com>
2023-03-25 13:46:11 +1100

    Upate code for Week 3

--- logging.js
+++
@@ -1,9 +0,0 @@
-var express = require('express')
-var app = express()
-
-app.use((req, res, next) => {
-    console.log("[Server Activity:] " + Date(Date.now()));
-    next();
-});
-
-app.listen(3000, () => console.log('Example app listening on port 3000!'));
